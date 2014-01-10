9310079944db095416f7a855716eff0e24be9b8e
Steven L. Speek <slspeek@gmail.com>
2014-01-11 00:12:23 +0100

    switching to xterm with autocutsel

--- init-script/bootstrap-dotfiles.sh
+++ init-script/bootstrap-dotfiles.sh
@@ -2,11 +2,12 @@ BACKUP=~/dotfiles.backup
 
 cd ~
 mkdir -vp $BACKUP
-mv -v .vimrc .vim .tmux.conf .bashrc $BACKUP
+mv -v .Xresources .vimrc .vim .tmux.conf .bashrc $BACKUP
 ln -s $HOME/dotfiles/.vimrc 
 ln -s $HOME/dotfiles/.vim 
 ln -s $HOME/dotfiles/.tmux.conf
 ln -s $HOME/dotfiles/.bashrc
+ln -s $HOME/dotfiles/.Xresources
 cd ~/dotfiles
 git submodule init
 git submodule update
