848658b5f817965033345d1ab47936e92b47e763
Steven L. Speek <slspeek@gmail.com>
2014-01-15 22:40:41 +0100

    fish related

--- init-script/bootstrap-dotfiles.sh
+++ init-script/bootstrap-dotfiles.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
 BACKUP=~/dotfiles.backup
 
 cd ~
 mkdir -vp $BACKUP
-mv -v .Xresources .vimrc .vim .tmux.conf .bashrc $BACKUP
+mv -v .Xresources .vimrc .vim .tmux.conf .bashrc ~/.config/fish $BACKUP
 ln -s $HOME/dotfiles/.vimrc 
 ln -s $HOME/dotfiles/.vim 
 ln -s $HOME/dotfiles/.tmux.conf
 ln -s $HOME/dotfiles/.bashrc
 ln -s $HOME/dotfiles/.Xresources
+cd ~/.config/
+ln -s $HOME/dotfiles/fish
+
 cd ~/dotfiles
 git submodule init
 git submodule update
