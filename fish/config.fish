4c7f943b270717b4491cdeff2fe69a5ca3d4a4b9
Steven L. Speek <slspeek@gmail.com>
2014-08-15 21:15:20 +0200

    parabola libre linux config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
-set -x GOROOT ~/tools/go
-set -x GOPATH ~/projs/camli
+set -x GOPATH ~/go
 set -x EDITOR vim
-set -x PATH $GOROOT/bin $GOPATH/bin ~/tools/bin $PATH
+set -x PATH $GOROOT/bin $GOPATH/bin ~/tools/bin $PATH /usr/local/bin
 
 #disable <C-s> in the shell
 stty -ixon
