58bd3e9c9ecd7156170601e4c2783597c0995b08
Steven L. Speek <slspeek@gmail.com>
2014-08-29 17:19:37 +0200

    Rather dwb then icecat

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ set -x GOPATH ~/go
 set -x EDITOR vim
 set -x PATH $GOROOT/bin $GOPATH/bin ~/tools/bin $PATH /usr/local/bin
 
+alias vi=vim
 #disable <C-s> in the shell
 stty -ixon
 setxkbmap -layout us -option ctrl:nocaps
