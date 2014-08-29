488af7222b2362fb48727181dd8950cff300e003
Steven L. Speek <slspeek@gmail.com>
2014-08-29 22:35:49 +0200

    vim alias

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ set -x EDITOR vim
 set -x PATH $GOROOT/bin $GOPATH/bin ~/tools/bin $PATH /usr/local/bin
 
 alias vi=vim
+alias sud='sudo su -'
 #disable <C-s> in the shell
 stty -ixon
 setxkbmap -layout us -option ctrl:nocaps
