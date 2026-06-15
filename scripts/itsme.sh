0f816a1209608e6e83cc8bba5869c222cd5a9ad3
Steven L. Speek <slspeek@gmail.com>
2026-06-15 14:13:11 +0200

    itsme script
    sets ssh remote for this repo
    gitconfig for Steven

---
+++ scripts/itsme.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+REPO_PATH=$(realpath "$0")/..
+
+function change_remote_origin() {
+    cd "$REPO_PATH" || exit
+    git remote set-url origin git@github.com/slspeek/dotfiles
+}
+
+function link_gitconfig() {
+    ln -s ~/"$REPO_PATH"/dot-gitconfig ~/.gitconfig
+}
+
+change_remote_origin
+link_gitconfig
