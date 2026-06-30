69abcf4b53ad9f0c5a2f707990409a20684b2a40
Steven <steven@localhost>
2026-06-30 13:38:11 +0200

    fix: url for dotfiles

--- scripts/itsme.sh
+++ scripts/itsme.sh
@@ -4,7 +4,7 @@ REPO_PATH=$(dirname "$(realpath "$0")")/..
 
 function change_remote_origin() {
     cd "$REPO_PATH" || exit
-    git remote set-url origin git@github.com/slspeek/dotfiles
+    git remote set-url origin git@github.com:slspeek/dotfiles
 }
 
 function link_gitconfig() {
