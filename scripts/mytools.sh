1bf39a56b5635e16bcf50c9dd33a895711e573e0
Steven L. Speek <slspeek@gmail.com>
2026-07-13 11:55:25 +0200

    Remove Zellij

--- scripts/mytools.sh
+++ scripts/mytools.sh
@@ -25,13 +25,3 @@ sudo apt-get install --yes \
 if ! command -v uv; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
-
-if ! command -v cargo; then
-    "$(dirname "$(realpath "$0")")"/install-cargo.sh
-    # shellcheck disable=SC1091
-    source "$HOME/.cargo/env"
-fi
-
-if ! command -v zellij; then
-    "$(dirname "$(realpath "$0")")"/install-zellij.sh
-fi
